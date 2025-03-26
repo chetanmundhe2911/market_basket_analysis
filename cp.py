63e94c363ae39ed224affa394b80e4adcc1789f0
Chetan <67324853+chetanmundhe2911@users.noreply.github.com>
2025-03-26 17:29:53 +0530

    Update cp.py

--- cp.py
+++ cp.py
@@ -22,3 +22,46 @@ ifelse(
 
     NULL -- Default return value if nothing matches
 )
+
+
+##-------------------------------------------------------------------------------------------------
+
+
+ifelse(
+    ${MetricToDisplay} = 'Unique Users',
+    ifelse(
+        distinct_count(ifelse({Organization_CP} = 'CrossPlay', {nr_sbsc_hash_pii}, NULL)) < 15, 
+        NULL, 
+        distinct_count(ifelse({Organization_CP} = 'CrossPlay', {nr_sbsc_hash_pii}, NULL))
+    ),
+
+    ${MetricToDisplay} = 'Tx Count',
+    ifelse(
+        ${crossPlay} = 'Yes',
+        sum(ifelse({Organization_CP} = 'CrossPlay', {tx_count}, NULL)),
+        sum({tx_count})  -- If "No" is selected, sum all transactions
+    ),
+
+    ${MetricToDisplay} = 'Tx Value',
+    ifelse(
+        ${crossPlay} = 'Yes',
+        sum(ifelse({Organization_CP} = 'CrossPlay', {tx_value}, NULL)),
+        sum({tx_value})  -- If "No" is selected, sum all transaction values
+    ),
+
+    ${MetricToDisplay} = 'All',
+    concat(
+        'Unique Users: ', toString(distinct_count(ifelse({Organization_CP} = 'CrossPlay', {nr_sbsc_hash_pii}, NULL))),
+        ', Tx Count: ', toString(sum(ifelse({Organization_CP} = 'CrossPlay', {tx_count}, NULL))),
+        ', Tx Value: ', toString(sum(ifelse({Organization_CP} = 'CrossPlay', {tx_value}, NULL)))
+    ),
+
+    NULL -- Default return in case no conditions match.
+)
+
+
+
+
+
+
+
