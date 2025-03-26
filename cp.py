d77c3f64a94e7555f9abaf41ed671d0069525467
Chetan <67324853+chetanmundhe2911@users.noreply.github.com>
2025-03-26 17:14:14 +0530

    Update cp.py

--- cp.py
+++ cp.py
@@ -5,20 +5,20 @@ ifelse(
         NULL, 
         distinct_count(ifelse({Organization_CP} = 'CrossPlay', {nr_sbsc_hash_pii}, NULL))
     ),
-    
+
     ${MetricToDisplay} = 'Tx Count',
     ifelse(
         ${crossPlay} = 'Yes',
         sum(ifelse({Organization_CP} = 'CrossPlay', {tx_count}, NULL)),
-        sum({tx_count})  -- Show total Tx count if "No" is selected
+        sum({tx_count}) -- If "No" is selected, sum all transactions
     ),
-    
+
     ${MetricToDisplay} = 'Tx Value',
     ifelse(
         ${crossPlay} = 'Yes',
         sum(ifelse({Organization_CP} = 'CrossPlay', {tx_value}, NULL)),
-        sum({tx_value})  -- Show total value if "No" is selected
+        sum({tx_value}) -- If "No" is selected, sum all transaction values
     ),
-    
-    NULL -- Default case if none of the conditions match
+
+    NULL -- Default return value if nothing matches
 )
