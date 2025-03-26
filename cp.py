f9c15e9c49c429c35c57941e4ee7957b559595f0
Chetan <67324853+chetanmundhe2911@users.noreply.github.com>
2025-03-26 17:06:41 +0530

    Update cp.py

--- cp.py
+++ cp.py
@@ -1,26 +1,24 @@
 ifelse(
     ${MetricToDisplay} = 'Unique Users',
     ifelse(
-        ${crossPlay} = 'Yes', 
-        ifelse(
-            distinct_count(ifelse({Organization_CP} = 'CrossPlay', {nr_sbsc_hash_pii}, NULL)) < 15, 
-            NULL, 
-            distinct_count(ifelse({Organization_CP} = 'CrossPlay', {nr_sbsc_hash_pii}, NULL))
-        ),
-        distinct_count({nr_sbsc_hash_pii}) -- If No is selected, counts all users
+        distinct_count(ifelse({Organization_CP} = 'CrossPlay', {nr_sbsc_hash_pii}, NULL)) < 15, 
+        NULL, 
+        distinct_count(ifelse({Organization_CP} = 'CrossPlay', {nr_sbsc_hash_pii}, NULL))
     ),
-
+    
     ${MetricToDisplay} = 'Tx Count',
     ifelse(
         ${crossPlay} = 'Yes',
         sum(ifelse({Organization_CP} = 'CrossPlay', {tx_count}, NULL)),
-        sum({tx_count})  -- If No is selected, sum all transactions
+        sum({tx_count})  -- Show total Tx count if "No" is selected
     ),
-
+    
     ${MetricToDisplay} = 'Tx Value',
     ifelse(
         ${crossPlay} = 'Yes',
         sum(ifelse({Organization_CP} = 'CrossPlay', {tx_value}, NULL)),
-        sum({tx_value})  -- If No is selected, sum all transaction values
-    )
+        sum({tx_value})  -- Show total value if "No" is selected
+    ),
+    
+    NULL -- Default case if none of the conditions match
 )
