396de76ba1076a26608848a522b885ade55a9d35
Chetan <67324853+chetanmundhe2911@users.noreply.github.com>
2025-03-26 17:03:04 +0530

    Update cp.py

--- cp.py
+++ cp.py
@@ -2,19 +2,25 @@ ifelse(
     ${MetricToDisplay} = 'Unique Users',
     ifelse(
         ${crossPlay} = 'Yes', 
-        ifelse(distinct_count(ifelse({Organization_CP} = 'CrossPlay', {nr_sbsc_hash_pii}, NULL)) < 15, 
-               NULL, 
-               distinct_count(ifelse({Organization_CP} = 'CrossPlay', {nr_sbsc_hash_pii}, NULL))
+        ifelse(
+            distinct_count(ifelse({Organization_CP} = 'CrossPlay', {nr_sbsc_hash_pii}, NULL)) < 15, 
+            NULL, 
+            distinct_count(ifelse({Organization_CP} = 'CrossPlay', {nr_sbsc_hash_pii}, NULL))
         ),
-        distinct_count({nr_sbsc_hash_pii})
+        distinct_count({nr_sbsc_hash_pii}) -- If No is selected, counts all users
     ),
 
     ${MetricToDisplay} = 'Tx Count',
     ifelse(
         ${crossPlay} = 'Yes',
         sum(ifelse({Organization_CP} = 'CrossPlay', {tx_count}, NULL)),
-        sum({tx_count})  -- Counts all Tx if No is selected
+        sum({tx_count})  -- If No is selected, sum all transactions
     ),
 
-    sum(ifelse({Organization_CP} = 'CrossPlay', {tx_value}, NULL))
+    ${MetricToDisplay} = 'Tx Value',
+    ifelse(
+        ${crossPlay} = 'Yes',
+        sum(ifelse({Organization_CP} = 'CrossPlay', {tx_value}, NULL)),
+        sum({tx_value})  -- If No is selected, sum all transaction values
+    )
 )
