694bae9e89b651b45c258b647fa5d89b10fe3e63
Chetan <67324853+chetanmundhe2911@users.noreply.github.com>
2025-05-31 19:34:56 +0530

    Update trading.py

--- trading.py
+++ trading.py
@@ -18,7 +18,15 @@ plotshape(t_12min_close + 0.1, title="12-min Close Dot", location=location.absol
 // Plot yellow dot for 14-min close
 // plotshape(t_14min_close, title="14-min Close Dot", location=location.absolute, style=shape.circle, size=size.tiny, color=color.yellow)
 
+#------------------------------------------------------------------------------------
+//@version=5
+indicator("NATGASMINI - Highlight 6PM to 11PM", overlay=true)
 
+// Get current hour and minute in exchange timezone
+t = time("1", "0600-1100")  // 6PM to 11PM
+
+// Plot background highlight
+bgcolor(t ? color.new(color.blue, 85) : na)
 
   
 
